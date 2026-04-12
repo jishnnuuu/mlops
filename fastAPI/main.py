d5711b450a8b67a1b9baec6675328de34076faba
Jishnu Satwik <jishnusatwikkcp@gmail.com>
2026-04-13 01:20:14 +0530

    implemented Query Parameters for sorting.

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 """
 Path() function in FastAPI is used to provide metadata, validation rules, 
 and documentation hints for path parameters in out API endpoints
@@ -41,3 +41,21 @@ def view_patient(patient_id: str = Path(..., description='ID of the patient in t
     if patient_id in data:
         return data[patient_id]
     return HTTPException(status_code=404, detail='Patient not found')
+
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description='sort on basis of height/weight/bmi'),
+                    order: str = Query('asc', description='sort in asc/ desc order')):
+    valid_fields = ['height', 'weight', 'bmi']
+    
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail='Invalid field select from {valid_fields}')
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail='Invalid field select from asc/desc')
+    
+    data = load_data()
+    
+    sort_order = True if order=='desc' else False
+    
+    sorted_data = sorted(data.values(), key = lambda x: x.get(sort_by, 0), reverse=sort_order)
+    
+    return sorted_data
